a297bc21f7aa2d6a0b13d82f8d8ec65365b8c7e9
Andrea-Pomati <pomati.andrea@gmail.com>
2023-02-24 19:46:51 +0100

    feat: content javascript

--- JS/script.js
+++ JS/script.js
@@ -32,14 +32,56 @@ Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
 
 
 */
+//creo variabile  assegno  prendihtml. da html cerca l'id  (id)   
+let ContainerEl = document.getElementById("_numbercontainer")
+console.log(ContainerEl)
 
-let numberContainer = document.getElementById("_numbercontainer")
-console.log(numberContainer)
-
-
+// sintassi (ciclo = for) (creo un indice con una variabile (let i) e assegno il valore (1); if condizione = true  esegui ciclo)
+// (i <= 100; = condizione), se false esce dal ciclo; aumenta la variabile che ho creato che funge da contatore di 1 tramite
+// il (i++), es se voglio aumentare di 2 (i +=2) salva su se stesso, se stesso + 2 e aumenta.
 for (let i = 1; i <= 100; i++) {
-console.log(i)
+    // console.log(i)
 
+    // creo variabile e la assegno a: vado su html creo un nuovo elemento di tipo ("tag")
+    let newEl = document.createElement("span");
+    // prendo la variabile creata all'inizio e uso il metodo append (variabile newEl appena creata)
+    // facendo così dico all'html che la nuova classe è figlia della prima.
+    ContainerEl.append(newEl);
+    // variabile (contenitore)  .vado ad inserire il valore nell'html (innerHTML) = valore da inserire (i) se voglio
+    // che cambia ad ogni ciclo.
+    newEl.innerHTML = i;
+    
+    // console.log(newEl.innerHTML);
+
+// se i è divisibile per 3 =paragonato a 0 (deve dare resto 0)
+    
+
+    if (i % 3 == 0 && i % 5 == 0){
+    newEl.innerHTML ="FizzBuzz"
+    console.log(newEl.innerHTML)
+    
+
+
+    } else if (i % 3 == 0) {
+        newEl.innerHTML = "Fizz";
+        console.log(newEl.innerHTML)
+
+
+
+    } else if (i % 5 == 0) {
+        newEl.innerHTML ="Buzz"
+        console.log(newEl.innerHTML)
+
+
+   
+
+    }else{
+    
+        console.log(newEl.innerHTML)
+
+
+
+    }
 
 
 
